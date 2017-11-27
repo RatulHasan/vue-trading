4c5b00677ee0079e4c93b8c14f00c9e27552b597
ratul <ratuljh@gmail.com>
2017-11-27 19:59:47 +0600

    Initial commit

---
+++ src/store/modules/stock.js
@@ -0,0 +1,38 @@
+import stocks from '../../../data/stocks'
+const state = {
+    stocks: []
+};
+
+const  mutations = {
+    'SET_STOCKS' (state, stocks) {
+        state.stocks = stocks;
+    },
+    'RAND_STOCKS' (state) {
+
+    }
+};
+
+const actions = {
+    buyStock: ({commit}, order) => {
+        commit('BUY_STOCK',order);
+    },
+    initStocks: ({commit}) => {
+        commit('SET_STOCKS', stocks)
+    },
+    randomizeStocks: ({commit}) => {
+        commit('RAND_STOCKS');
+    }
+};
+
+const getters = {
+    stocks: state => {
+        return state.stocks;
+    }
+};
+
+export default  {
+    state,
+    mutations,
+    actions,
+    getters
+}
